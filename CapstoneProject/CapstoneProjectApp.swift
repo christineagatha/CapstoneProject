7c52d7b00c998188b4332477227ca4008114dcda
christineagatha <christinejiang09@gmail.com>
2025-05-17 16:49:58 -0400

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by christine j on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
