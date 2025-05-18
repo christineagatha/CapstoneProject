929ad688af43019d14bc5ace31f0937a6ed01a3a
christineagatha <christinejiang09@gmail.com>
2025-05-17 22:29:45 -0400

    general layout of root view, need to decide what content to add

--- CapstoneProject/ContentView.swift
+++ CapstoneProject/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationStack {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            (Text("Tune ") + Text(Image(systemName: "music.note")) + Text("n"))
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.teal)
+                .padding(.top, 40)
+            Image("piano")
+                .padding(10)
+            Text("sdvgsvs")
+            Spacer()
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+            }
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+            }
+            Spacer()
+            Text("@Copyright 2025 TuneIn")
         }
         .padding()
+        }
+
     }
 }
 
